75765f42ca9ca536f5def290e8a4dd81e8bee02c
yashshinde0080 <syash0080@gmail.com>
2026-01-29 18:56:54 +0530

    29/1/2026

---
+++ landing_page/src/App.tsx
@@ -0,0 +1,23 @@
+// import { Button } from "@/components/ui/button"
+import Hero from "./components/hero"
+import Features from "./components/features"
+import Integrations from "./components/integrations-7"
+import Content from "./components/content-5"
+import CallToAction from "./components/call-to-action"
+import Footer from "./components/footer"
+import Faqs from "./components/faqs-3"
+function App() {
+  return (
+    <>
+    <Hero /> 
+    <CallToAction />
+    <Features />
+    <Integrations />
+    <Content />
+    <Faqs />
+    <Footer />
+    </>
+  )
+}
+
+export default App
