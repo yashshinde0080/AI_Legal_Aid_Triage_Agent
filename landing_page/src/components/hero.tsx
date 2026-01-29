dddb8bf086608fed28d7963ea89d71944fe14f73
yashshinde0080 <syash0080@gmail.com>
2026-01-29 18:56:54 +0530

    29/1/2026

---
+++ landing_page/src/components/hero.tsx
@@ -0,0 +1,45 @@
+import { ArrowUpRight, CirclePlay } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+  return (
+    <div className="flex min-h-screen items-center justify-center overflow-hidden">
+      <div className="mx-auto grid w-full max-w-(--breakpoint-xl) gap-12 px-6 py-12 lg:grid-cols-2 lg:py-0">
+        <div className="my-auto">
+          <Badge
+            asChild
+            className="rounded-full border-border py-1"
+            variant="secondary"
+          >
+            <Link to="#">
+              Just released v1.0.0 <ArrowUpRight className="ml-1 size-4" />
+            </Link>
+          </Badge>
+          <h1 className="mt-6 max-w-[17ch] font-semibold text-4xl leading-[1.2]! tracking-[-0.035em] md:text-5xl lg:text-[2.75rem] xl:text-[3.25rem]">
+            Customized Shadcn UI Blocks & Components
+          </h1>
+          <p className="mt-6 max-w-[60ch] text-foreground/80 text-lg">
+            Explore a collection of Shadcn UI blocks and components, ready to
+            preview and copy. Streamline your development workflow with
+            easy-to-implement examples.
+          </p>
+          <div className="mt-12 flex items-center gap-4">
+            <Button className="rounded-full text-base" size="lg">
+              Get Started <ArrowUpRight className="h-5! w-5!" />
+            </Button>
+            <Button
+              className="rounded-full text-base shadow-none"
+              size="lg"
+              variant="outline"
+            >
+              <CirclePlay className="h-5! w-5!" /> Watch Demo
+            </Button>
+          </div>
+        </div>
+        <div className="aspect-video w-full rounded-xl bg-accent lg:aspect-auto lg:h-[calc(100vh-4rem)] lg:w-[1000px]" />
+      </div>
+    </div>
+  );
+}
